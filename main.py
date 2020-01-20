c6f1342396c5251ed578584a331d41610dc32c04
Helle Gråberg <helle.graaberg@hotmail.com>
2020-01-20 12:57:34 +0100

    Add main

---
+++ main.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    print('Welcome to AI programming project 1 :)')
+    Menu = {
+        -1: 'Testspace',
+        '1': 'Run',
+
+    if Menu == 'Testspace':
+        print('Welcome to testspace')
+
+    elif Menu == 'Diamond Board':
+        print('Running program...')
