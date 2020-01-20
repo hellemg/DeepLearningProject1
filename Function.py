09644c04662192cfa83fbe47ddc469b7d4d91ad4
Helle Gråberg <helle.graaberg@hotmail.com>
2020-01-20 14:59:42 +0100

    Add Function-class with simple RelU

---
+++ Function.py
@@ -0,0 +1,26 @@
+class Function:
+    def __init__(self):
+        self.name = 'hei'
+
+    def do_the_math(sself, value):
+        raise NotImplementedError
+
+    def derivative(self, value):
+        """
+        Return derivate of the value
+        """
+        raise NotImplementedError
+
+class ReLU(Function):
+    def __init__(self):
+        super().__init__()
+
+    def derivative(self, value):
+        if value <= 0:
+            return 0
+        else:
+            return 1
+
+    def do_the_math(self, value):
+        return max(0, value)
+
