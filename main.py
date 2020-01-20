e43589fb3d1f986466cb98ed604a5caecd428dd0
Helle Gråberg <helle.graaberg@hotmail.com>
2020-01-20 14:10:20 +0100

    Read training data. Onehot encode labels

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import configparser
+import numpy as np
+import csv
+
+"""
+The following imports are OK, and not anything else: numpy, matplotlib.pyplot, configparser, enum,
+sys and softmax from scipy.special. Notice that tanh is available from numpy.
+"""
 
 if __name__ == '__main__':
     Menu = {
         -1: 'Testspace',
         1: 'Run',
         2: 'Create config',
-        3: 'Read config'
-    }[3]
+        3: 'Read config',
+        4: 'Transform data'
+    }[4]
 
     if Menu == 'Testspace':
         print('Welcome to testspace')
@@ -24,11 +32,11 @@ if __name__ == '__main__':
         config['DATA'] = {'training': './DATA/train_small.csv',
                           'validation': './DATA/validate_small.csv'}
         config['MODEL'] = {'layers': ' 24, 12,6',
-                            'activations': 'relu, relu, tanh',
-                            'loss_type': 'cross_entropy'}
+                           'activations': 'relu, relu, tanh',
+                           'loss_type': 'cross_entropy'}
         config['HYPER'] = {'learning_rate': '5.E-2',
-                            'no_epochs': '10',
-                            'L2_regularization': '5.E-3'}
+                           'no_epochs': '10',
+                           'L2_regularization': '5.E-3'}
         with open('config.ini', 'w') as configfile:
             config.write(configfile)
 
@@ -37,7 +45,7 @@ if __name__ == '__main__':
         config = configparser.ConfigParser()
         config.read('config.ini')
         print(config.sections())
-        
+
         for section in config.sections():
             print('---'+section)
             for key in config[section]:
@@ -54,8 +62,34 @@ if __name__ == '__main__':
         # TODO: if layers[0] == 0, no hidden layers
         activations = config['MODEL']['activations']
         loss_type = config['MODEL']['loss_type']
+        """ TODO:
+        Note that in
+        addition to the mentioned functions, you will also need to handle the softmax as part of your
+        implementation to handle the classification-loss (see below).
+        """
 
         # HYPER
         learning_rate = config['HYPER']['learning_rate']
         no_epochs = config['HYPER']['no_epochs']
         L2_regularization = config['HYPER']['L2_regularization']
+
+    elif Menu == 'Transform data':
+        print('Transforming data...')
+        path = './DATA/train_small.csv'
+        x_train = []
+        y_train = []
+
+        with open(path) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                x_train.append([int(float(x)) for x in row[:-1]])
+                y_train.append(int(float(row[-1])))
+        x_train = np.array(x_train)
+        y_train = np.array(y_train)
+
+        no_examples = len(y_train)
+        no_classes = max(y_train) + 1
+
+        one_hot = np.zeros((no_examples, no_classes))
+        # Uses the i'th entry in each array at the same time
+        one_hot[np.arange(no_examples), y_train] = 1
